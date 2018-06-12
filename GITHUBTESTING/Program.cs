752d19ee132f297e1fd681ed9d968f80b015de1b
Adrian Borgen <exponentialname@outlook.com>
2018-06-12 10:11:52 +0200

    first commit

---
+++ GITHUBTESTING/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GITHUBTESTING
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
