64cc5b1b67b72f2d979f9aaa5b64779a04ab1705
Adrian Borgen <exponentialname@outlook.com>
2018-06-13 08:34:55 +0200

    get set advancement

--- GITHUBTESTING/Program.cs
+++ GITHUBTESTING/Program.cs
@@ -10,9 +10,15 @@ namespace GITHUBTESTING
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("TESTING TEST");
-            Console.WriteLine("How are you today? ");
-            Console.WriteLine("HELLOHELOHELO");
+            Dog D1 = new Dog("Bigdog", "12", "Brazil");
+            Cat C1 = new Cat("Tigermancat", "42", "Norway'nt", "Meooooooooow");
+            Fish F1 = new Fish("Sea Linguini", "124", "Sealand");
+            Bird B1 = new Bird("Eaglebird", "4", "Afrika'nt", "Pipipipipipipipipip");
+
+            Console.WriteLine(D1.FuldDyreInfo());
+            Console.WriteLine(C1.FuldDyreInfo());
+            Console.WriteLine(F1.FuldDyreInfo());
+            Console.WriteLine(B1.FuldDyreInfo());
         }
     }
 }
